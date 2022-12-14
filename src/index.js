1440756e6f22a4e69c5d3f96421abaeb7b518e9b
fernandosantoshs <fernandosantoshs@gmail.com>
2022-12-14 16:23:44 -0300

    Added route post account;

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ const app = express()
 
 app.use(express.json())
 
+const customers = [{
+    name: "Bill", 
+    cpf: "123456789"
+}]
+
+app.post('/account', (request, response) => {
+    const { name , cpf } = request.body;
+
+    const userExists = customers.some( customer => customer.cpf === cpf)
+
+    if(userExists) {
+        return response.status(400).json({error: "User already exists"})
+    }
+
+    customers.push({
+        cpf: cpf,
+        name:name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    response.status(201).send({message: "Account created Successfully"})
+});
+
+app.listen(3333);
