2aa66a843bbfb79c5b3dd962ff200372aac1c3eb
fernandosantoshs <fernandosantoshs@gmail.com>
2022-12-20 09:20:28 -0300

    Added route get statement; middleware to check user existance

--- src/index.js
+++ src/index.js
@@ -5,14 +5,28 @@ const app = express()
 app.use(express.json())
 
 const customers = [{
-    name: "Bill", 
-    cpf: "123456789"
+    cpf: "123456789",
+    name: "Bill",
+    id:"testID",
+    statement: []
 }]
+
+function checkUserExistance(request, response, next) {
+    const { cpf } = request.headers
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer) return response.status(404).json({message: "User not found!"})
+
+    request.customer = customer
+
+    return next()
+}
 
 app.post('/account', (request, response) => {
     const { name , cpf } = request.body;
 
-    const userExists = customers.some( customer => customer.cpf === cpf)
+    const userExists = customers.some(customer => customer.cpf === cpf)
 
     if(userExists) {
         return response.status(400).json({error: "User already exists"})
@@ -25,7 +39,19 @@ app.post('/account', (request, response) => {
         statement: []
     })
 
-    response.status(201).send({message: "Account created Successfully"})
+    return response.status(201).send({message: "Account created Successfully"})
 });
+
+app.get('/statement', checkUserExistance, (request, response) => {
+    const customer = request
+
+    return response.status(200).json(customer.statement)
+})
+
+app.post('/deposit', checkUserExistance, (request, response) => {
+    const customer = request
+    const { description, amount } = request.body
+
+})
 
 app.listen(3333);
